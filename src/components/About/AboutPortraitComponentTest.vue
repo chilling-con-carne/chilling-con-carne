d7842d8d159d2c2c03ac7d9e057ce108d0895641
Vincent WENDLING <vincent.wendling@etu.unistra.fr>
2022-05-19 13:41:04 +0200

    :construction: trying to find out where the dumbass bug comes from

---
+++ src/components/About/AboutPortraitComponentTest.vue
@@ -0,0 +1,11 @@
+<template>
+	<h2>Test</h2>
+</template>
+
+<script>
+	
+</script>
+
+<style lang="scss" scoped>
+	
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -8,63 +8,22 @@
     <div class="radial-gradient"></div>
     <div class="radial-gradient"></div>
   </section>
-  <section id="about-portraits">
+  <!-- <section id="about-portraits">
     <AboutPortrait
       v-for="about in about_array"
       :about="about"
       :key="about.name"
     />
-  </section>
+  </section> -->
+  <AboutPortraitComponentTest />  
 </template>
 
 <script setup>
 import Chevrons from "@/components/Global/Chevrons.vue";
-import AboutPortrait from "@/components/About/AboutPortraitComponent.vue";
+//import AboutPortrait from "@/components/About/AboutPortraitComponent.vue";
+import AboutPortraitComponentTest from "@/components/About/AboutPortraitComponentTest.vue"
+// import { reactive } from "vue";
 
-import { reactive } from "vue";
-
-let about_array = reactive([
-  {
-    name: "Vincent Wendling",
-    position: "co-founder - ceo",
-    line: "Software engineer during the day, meme lord at night",
-    description:
-      "As students, we often develop skills that we can't use at their full potential until we graduated and found a job. With ChillingConCarne, we aspire to use our talents not only to meet our clients' expectations, but hopefully to exceed them. Our team members are versatile and we've got every skill needed to turn your idea into reality.",
-    picture: "logo.png",
-    linkedin: "https://www.linkedin.com/in/vincent-wendling-5a939a1b7/",
-    github: "https://github.com/TisaneFruitRouge",
-  },
-  {
-    name: "Germain Eguina",
-    position: "co-founder - fullstack dev",
-    line: "Wise, Smart & versatile",
-    description:
-      "I am 20 years old and I am from the Central African Republic. Currently studying computer engineering in Strasbourg, I have developed a versatility in development and programming. I like to spend time on a subject that interests me and new technologies and computer science itself are great passions.",
-    picture: "logo.png",
-    linkedin: "https://www.linkedin.com/in/germain-eguina-4bab70220/",
-    github: "https://github.com/kiLLuua",
-  },
-  {
-    name: "Eden Kharoub",
-    position: "co-founder - designer",
-    line: "Cat enthusiast with a passion for detail.",
-    description:
-      "I’m a frontend engineer passionate about graphic design and data visualisation. I like playing piano, cooking, gardening, and just trying every possible art medium known to man. I love learning new things and my coworkers like to say I have an unhealthy obsession with reading documentation. I currently am in a five-year integrated masters degree course studying computer science, focusing primarily on graphics programming, VR, and game interaction.",
-    picture: "logo.png",
-    linkedin: "https://fr.linkedin.com/in/eden-kharoub-6a9958187",
-    github: "https://github.com/Kokoden",
-  },
-  {
-    name: "Léandre BLANCHEGELEY",
-    position: "co-founder - backend dev",
-    line: "Manliest man you've ever met",
-    description:
-      "I am a 20 years old student at 42 School and a technology lover, I thrive on innovating and discovering new things. As a back end programmer, exploring the depth of systems and building their architecture is one of my many skills.",
-    picture: "photos/leandre.jpeg",
-    linkedin: "https://www.linkedin.com/in/l%C3%A9andre-blanchegeley-618152224/",
-    github: "https://github.com/SanGoruden",
-  },
-]);
 </script>
 
 <style lang="scss" scoped>
